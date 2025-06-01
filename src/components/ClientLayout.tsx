a9eb30676798577db903b6283016f55c0feae527
Tomás Maritano <tomasmaritano@Tomass-MacBook-Pro-2.local>
2025-06-01 16:07:58 -0300

    clientlayout mod

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -1,7 +1,7 @@
 // src/components/ClientLayout.tsx
 'use client';
 import { ReactNode } from 'react';
-import Web3Wrapper from './Web3Wrapper';
+import { Web3Wrapper } from './Web3Wrapper';
 
 export default function ClientLayout({ children }: { children: ReactNode }) {
   return <Web3Wrapper>{children}</Web3Wrapper>;
