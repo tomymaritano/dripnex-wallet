596833c783712ea4ac8dc6fc5ee3f4a7c37c9835
tomymaritano <tomymaritano@gmail.com>
2025-06-07 23:37:05 -0300

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,30 @@
+// src/middleware.ts
+import type { NextRequest } from 'next/server'
 import { setupCsrf } from './src/lib/csrf'
 
-export const middleware = setupCsrf()
+const csrfMiddleware = setupCsrf()
+
+export function middleware(req: NextRequest) {
+  const res = csrfMiddleware(req)
+
+  // Dynamic CSP depending on environment
+
+  const csp = `
+    default-src 'self';
+    script-src 'self' 'unsafe-inline' https:;
+    style-src 'self' 'unsafe-inline' https:;
+    img-src * blob: data:;
+    connect-src *;
+    font-src 'self' https: data:;
+    object-src 'none';
+    frame-ancestors 'none';
+  `.replace(/\s{2,}/g, ' ').trim()
+
+  res.headers.set('Content-Security-Policy', csp)
+
+  return res
+}
 
 export const config = {
-  matcher: ['/api/:path*'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 }
